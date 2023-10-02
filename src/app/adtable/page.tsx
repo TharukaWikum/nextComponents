660f1d01b9d033a8453ad1dbdc76f209ed248412
TharukaWikum <tharuka99wikum@mail.com>
2023-10-02 16:34:52 +0530

    table component avatar, edit icon

--- src/app/adtable/page.tsx
+++ src/app/adtable/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import ReusableTable from "../components/table/adtable";
 import data from "./data"; // Import your data source
+import { UserOutlined } from "@ant-design/icons";
 
 function Page() {
   const columns = [
@@ -15,6 +16,7 @@ function Page() {
       title: "Name",
       dataIndex: "name", // This should match the key in your data
       key: "name",
+      width: 150, // you can provide the width
     },
     {
       title: "Description",
@@ -28,24 +30,34 @@ function Page() {
 
   const columnTags = {
     id:true,
-    name: true, // Render "Name" column as a Tag
+    
+    // name: true, // Render "Name" column as a Tag
     // Add more columns and their tag rendering flags as needed
+  };
+
+  const iconColumns = {
+    name: <UserOutlined />, // Map the "name" column to the UserOutlined icon
+    
   };
 
   return (
     <div>
       <ReusableTable
-        data={data}
-        columns={columns}
-        pageSize={pageSize}
-        pgVariant='pgv4'
-        onSelectRow={(selectedRow) => {
-          console.log('Selected Row:', selectedRow);
-        }}
-        columnTags={columnTags} // Pass the columnTags prop
-      />
+  data={data}
+  columns={columns}
+  pageSize={pageSize}
+  pgVariant='pgv4'
+  onSelectRow={(selectedRow) => {
+    console.log('Selected Row:', selectedRow);
+  }}
+  columnTags={columnTags}
+  iconColumns={iconColumns}
+/>
     </div>
   );
 }
 
 export default Page;
+
+
+
--- src/app/components/table/adtable.tsx
+++ src/app/components/table/adtable.tsx
@@ -1,12 +1,135 @@
 
 
+// import React, { useState } from 'react';
+// import { Table, Tag } from 'antd'; // Import Tag component
+// import { ColumnProps } from 'antd/lib/table';
+// import CustomPagination from './Pagination';
+// import './adtable.css';
+// import { Avatar} from 'antd';
+
+// // Define a type constraint for the 'T' type parameter
+// type AnyObject = Record<string, any>;
+
+// interface ReusableAdTableProps<T extends AnyObject> {
+//   data: T[];
+//   columns: ColumnProps<T>[];
+//   pageSize?: number;
+//   pgVariant?: string;
+//   onSelectRow?: (selectedRowKeys: React.Key[], selectedRows: T[]) => void;
+//   columnTags?: Record<string, boolean>; // Prop for specifying column tags
+//   iconColumns?: Record<string, React.ReactNode>; // Prop for specifying column icons
+// }
+
+
+// function ReusableAdTable<T extends AnyObject>(props: ReusableAdTableProps<T>) {
+//   const { data, columns, pgVariant, pageSize = 10, onSelectRow ,columnTags, iconColumns, } = props;
+
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+//   const [selectedRows, setSelectedRows] = useState<T[]>([]);
+
+//   const total = data.length; // Calculate the total based on dataSource
+
+//   const onPageChange = (page: number) => {
+//     setCurrentPage(page);
+//     // You can perform any other actions related to pagination here
+//   };
+
+//   const handleRowSelectionChange = (selectedRowKeys: React.Key[], selectedRows: T[]) => {
+//     setSelectedRowKeys(selectedRowKeys);
+//     setSelectedRows(selectedRows);
+//     if (onSelectRow) {
+//       onSelectRow(selectedRowKeys, selectedRows);
+//     }
+//   };
+
+//   const pageData = data
+//   .slice((currentPage - 1) * pageSize, currentPage * pageSize)
+//   .map((item) => {
+//     const modifiedItem: any = { ...item, key: item.id };
+//     for (const column of columns) {
+//       const dataIndex = column.dataIndex as keyof T;
+//       if (columnTags?.[column.key as string]) {
+//         modifiedItem[dataIndex] = (
+//           <Tag className='label'>{item[dataIndex]}</Tag>
+//         );
+//       }
+//       if (iconColumns && iconColumns[column.key as string]) {
+//         // Check if the current column has an associated icon
+//         modifiedItem[dataIndex] = (
+//           <>
+//             <Avatar className='avatar' icon={iconColumns[column.key as string]} /> {item[dataIndex]}
+//           </>
+//         );
+//       }
+//     }
+//     return modifiedItem as T;
+//   });
+
+
+//   const rowSelection = {
+//     selectedRowKeys,
+//     onChange: handleRowSelectionChange,
+//   };
+
+//   const paginationConfig = {
+//     pageSize,
+//     total,
+//     current: currentPage,
+//     onChange: onPageChange,
+//   };
+
+//   // Define a function to determine row class based on selection
+//   const getRowClassName = (record: T) => {
+//     if (selectedRowKeys.includes(record.key)) {
+//       return 'selected-row';
+//     }
+//     return '';
+//   };
+
+//   // Define the handleRowClick function
+//   const handleRowClick = (record: T) => {
+//     // Implement your logic for row click here
+//     console.log('Row clicked:', record);
+//   };
+
+//   return (
+//     <>
+//      <Table<T>
+//   dataSource={pageData}
+//   columns={columns}
+//   pagination={false}
+//   rowSelection={rowSelection}
+//   rowClassName={(record) => (selectedRowKeys.includes(record.key) ? 'selected-row' : '')}
+//   onRow={(record) => ({
+//     onClick: () => handleRowClick(record),
+//   })}
+// />
+//       <div className="pagination-container">
+//         <CustomPagination {...paginationConfig} pgVariant={pgVariant} />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default ReusableAdTable;
+
+
+
+
+
+
+
+
+
+
 
 import React, { useState } from 'react';
-import { Table, Tag } from 'antd'; // Import Tag component
+import { Table, Tag, Avatar } from 'antd'; // Import Tag component
 import { ColumnProps } from 'antd/lib/table';
 import CustomPagination from './Pagination';
 import './adtable.css';
-
+import { EditFilled } from "@ant-design/icons";
 
 // Define a type constraint for the 'T' type parameter
 type AnyObject = Record<string, any>;
@@ -17,15 +140,18 @@ interface ReusableAdTableProps<T extends AnyObject> {
   pageSize?: number;
   pgVariant?: string;
   onSelectRow?: (selectedRowKeys: React.Key[], selectedRows: T[]) => void;
-  columnTags?: Record<string, boolean>; // New prop for specifying column tags
+  columnTags?: Record<string, boolean>; // Prop for specifying column tags
+  iconColumns?: Record<string, React.ReactNode>; // Prop for specifying column icons
 }
 
+
 function ReusableAdTable<T extends AnyObject>(props: ReusableAdTableProps<T>) {
-  const { data, columns, pgVariant, pageSize = 10, onSelectRow ,columnTags } = props;
+  const { data, columns, pgVariant, pageSize = 10, onSelectRow ,columnTags, iconColumns, } = props;
 
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
   const [selectedRows, setSelectedRows] = useState<T[]>([]);
+  const [checkedRowKeys, setCheckedRowKeys] = useState<React.Key[]>([]);
 
   const total = data.length; // Calculate the total based on dataSource
 
@@ -53,9 +179,40 @@ function ReusableAdTable<T extends AnyObject>(props: ReusableAdTableProps<T>) {
           <Tag className='label'>{item[dataIndex]}</Tag>
         );
       }
+      if (iconColumns && iconColumns[column.key as string]) {
+        // Check if the current column has an associated icon
+        modifiedItem[dataIndex] = (
+          <>
+            <Avatar className='avatar' icon={iconColumns[column.key as string]} /> {item[dataIndex]}
+          </>
+        );
+      }
     }
     return modifiedItem as T;
   });
+
+
+
+
+
+
+
+
+  const editColumn: ColumnProps<T> = {
+    title: " ",
+    dataIndex: "edit",
+    key: "edit",
+    render: (text, record) => (
+      <EditFilled onClick={() => handleEditClick(record)} className="edit-icon" />
+    ),
+  };
+
+
+  const handleEditClick = (record: T) => {
+    // Implement your logic for the edit button click here
+    console.log('Edit clicked for record:', record);
+  };
+
 
   const rowSelection = {
     selectedRowKeys,
@@ -78,21 +235,40 @@ function ReusableAdTable<T extends AnyObject>(props: ReusableAdTableProps<T>) {
   };
 
   // Define the handleRowClick function
+  const onRowClick = (record: T) => {
+    const recordKey = record.key;
+    const updatedSelectedRowKeys = selectedRowKeys.includes(recordKey)
+      ? selectedRowKeys.filter((key) => key !== recordKey)
+      : [...selectedRowKeys, recordKey];
+    const updatedCheckedRowKeys = checkedRowKeys.includes(recordKey)
+      ? checkedRowKeys.filter((key) => key !== recordKey)
+      : [...checkedRowKeys, recordKey];
+    setSelectedRowKeys(updatedSelectedRowKeys);
+    setCheckedRowKeys(updatedCheckedRowKeys);
+  };
+
+
+  const updatedColumns = [...columns, editColumn];
+
   const handleRowClick = (record: T) => {
     // Implement your logic for row click here
     console.log('Row clicked:', record);
   };
 
+
   return (
     <>
      <Table<T>
   dataSource={pageData}
-  columns={columns}
+  columns={updatedColumns}
   pagination={false}
   rowSelection={rowSelection}
   rowClassName={(record) => (selectedRowKeys.includes(record.key) ? 'selected-row' : '')}
   onRow={(record) => ({
-    onClick: () => handleRowClick(record),
+    onClick: () => {
+      handleRowClick(record);
+      onRowClick(record);
+    },
   })}
 />
       <div className="pagination-container">
@@ -103,3 +279,36 @@ function ReusableAdTable<T extends AnyObject>(props: ReusableAdTableProps<T>) {
 }
 
 export default ReusableAdTable;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
